944b5f2e6ec970bf2fb0718707e0b1f53b1c18e7
Yogesh Upadhyay <114800361+Yogesh051@users.noreply.github.com>
2024-11-29 22:49:24 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import UserCard from './components/UserCard'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='container'>
-      <UserCard name="Virat" style={{"border-radius":"10px"}}/>
-      <UserCard name="Rishabh"/>
-      <UserCard name="Yogesh"/>
-    </div>
+    <>
+      <div>
+        <Counter/>
+      </div>
+        
+    </>
   )
 }
- 
+
 export default App
--- main.jsx
+++ main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import UserCard from './components/UserCard.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <App/>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
