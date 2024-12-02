bb6291102979d60545a454fa84886d0e8408f0f4
Yogesh Upadhyay <114800361+Yogesh051@users.noreply.github.com>
2024-12-03 02:09:42 +0530

    Add files via upload

--- Cards.jsx
+++ Cards.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Cards = (props) => {
   return (
     <div>
-      {props.name}
-      {props.children}
+      <input type='text' onChange={(e)=>props.setName(e.target.value)}/>
+      <p>Name value inside card: {props.name}</p>
     </div>
   )
 }
 
 export default Cards
+
