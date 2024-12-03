5c4efe5b12f6fec7e3591e23840070f0fdc28693
Yogesh Upadhyay <114800361+Yogesh051@users.noreply.github.com>
2024-12-03 23:45:30 +0530

    Add files via upload

---
+++ LogInBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogInBtn = () => {
+  return (
+   <button>LogIn</button>
+  )
+} 
+
+export default LogInBtn
---
+++ LogOutBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogOutBtn = () => {
+  return (
+   <button>LogOut</button>
+  )
+}
+
+export default LogOutBtn
